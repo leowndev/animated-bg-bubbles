eadba5ba527096ba9319d26810e27a915222054a
leowndev <leonardogt2308@gmail.com>
2024-01-14 21:09:50 -0300

    Create custom cursor bubble

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-import './style.scss';
+import "./style.scss";
+
+let curX: number = 0;
+let curY: number = 0;
+
+document.addEventListener("DOMContentLoaded", (): void => {
+  const interBubble: HTMLDivElement | null =
+    document.querySelector<HTMLDivElement>(".cursor");
+
+  function move(): void {
+    interBubble.style.transform = `translate(${curX}px, ${curY}px`;
+
+    requestAnimationFrame((): void => {
+      move();
+    });
+  }
+
+  window.addEventListener("mousemove", (event: MouseEvent) => {
+    curX = event.clientX;
+    curY = event.clientY;
+  });
+
+  move();
+});
